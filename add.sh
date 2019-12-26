06b58bc63db140eb09345dbe084282a664325456
2jj <58620248+2jj@users.noreply.github.com>
2019-12-26 15:29:07 +0100

    Update add.sh

--- add.sh
+++ add.sh
@@ -48,6 +48,9 @@ apt upgrade -y
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # npm
 nvm install --lts
